c795af36f783bfb90c919d99c4b15bd727698ea8
Venkata Krishna Rohit Sakala <rohitsakala@gmail.com>
2021-10-12 22:15:50 +0530

    Make FyerBroker compaitable with Broker interface
    Also add fyer into factory method of brokers

--- pkg/httpClient/flyerClient.go
+++ pkg/httpClient/flyerClient.go
@@ -10,32 +10,32 @@ import (
 )
 
 type Client interface {
-	GetQuote(symbol string) (SymbolQuote, error){
+	GetQuote(symbol string) (SymbolQuote, error)
 }
 
-type flyerClient struct{
-	client *http.Client
-	url string
-	appId string
+type flyerClient struct {
+	client      *http.Client
+	url         string
+	appId       string
 	accessToken string
 }
 
-func NewFyerHttpClient(url string) Client{
+func NewFyerHttpClient(url string) Client {
 	return &flyerClient{client: new(http.Client), url: url}
 }
 
-func (c *flyerClient) GetQuote(symbol string) (SymbolQuote, error){
-	req, err := http.NewRequest("GET", c.url + "/quotes/symbol=" + symbol, nil)
-	if err != nil{
+func (c *flyerClient) GetQuote(symbol string) (SymbolQuote, error) {
+	req, err := http.NewRequest("GET", c.url+"/quotes/symbol="+symbol, nil)
+	if err != nil {
 		return SymbolQuote{}, err
 	}
 	req.Header.Add("Authorization", strings.Join([]string{c.appId, c.accessToken}, ":"))
 	resp, err := c.client.Do(req)
-	if err != nil{
+	if err != nil {
 		return SymbolQuote{}, err
 	}
-	if resp.StatusCode != http.StatusOK{
-		return SymbolQuote{}, errors.New(fmt.Sprintf("unsuccessful response oode - %v", resp))
+	if resp.StatusCode != http.StatusOK {
+		return SymbolQuote{}, fmt.Errorf(fmt.Sprintf("unsuccessful response oode - %v", resp))
 	}
 	rp := make(map[string]json.RawMessage)
 	body, err := ioutil.ReadAll(resp.Body)
@@ -43,15 +43,15 @@ func (c *flyerClient) GetQuote(symbol string) (SymbolQuote, error){
 		return SymbolQuote{}, err
 	}
 	err = json.Unmarshal(body, &rp)
-	if err != nil{
+	if err != nil {
 		return SymbolQuote{}, err
 	}
-	if _, ok := rp["s"]; !ok{
+	if _, ok := rp["s"]; !ok {
 		return SymbolQuote{}, errors.New("unsuccessful status code from fyer client")
 	}
 	var status string
 	err = json.Unmarshal(rp["s"], &status)
-	if err!= nil{
+	if err != nil {
 		return SymbolQuote{}, err
 	}
 	if status != "ok" {
